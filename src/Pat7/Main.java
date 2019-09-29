b0d9f9f1687f4862b844ee2e47811817ac03abe4
nathan_li <453539707@qq.com>
2019-09-29 13:49:52 +0800

    update Pat 7

---
+++ src/Pat7/Main.java
@@ -0,0 +1,38 @@
+package Pat7;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        if (num < 100000) {
+            int res = 0;
+            Map<Integer, String> map = new HashMap<>();
+            for (int i = 3; i <= num - 2; i++) {
+                String a = i + "|" + (i + 2);
+                map.put(i, a);
+            }
+            for (int i = 3; i <= num - 2; i++) {
+                String[] str = map.get(i).split("\\|");
+                int j = Integer.parseInt(str[0]);
+                int k = Integer.parseInt(str[1]);
+                if (isPrime(j) && isPrime(k)) {
+                    res++;
+                }
+            }
+            System.out.println(res);
+        }
+    }
+    public static Boolean isPrime (Integer a) {
+        Boolean res = true;
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                res = false && res;
+            }
+        }
+        return res;
+    }
+}
